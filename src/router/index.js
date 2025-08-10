71708f10b113862fa6e4689682ddb15b85f15823
Diana Kondakova <di.d.kondakova@labmedia.su>
2025-08-10 12:07:52 +0300

    Set dev-server config

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,14 @@ import Notes from '@/views/notes.vue';
 export const ROUTE_HOME_NAME = 'Home';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      name: ROUTE_HOME_NAME,
-      path: '/home',
-      component: Notes,
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            name: ROUTE_HOME_NAME,
+            path: '/home',
+            component: Notes,
+        },
+    ],
 });
 
 export default router;
