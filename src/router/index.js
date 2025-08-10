757891af16f5687584dbc4c4450c13c97b256edb
Diana Kondakova <di.d.kondakova@labmedia.su>
2025-08-10 11:44:40 +0300

    Create app scaffold

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Notes from '@/views/notes.vue';
+
+export const ROUTE_HOME_NAME = 'Home';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      name: ROUTE_HOME_NAME,
+      path: '/home',
+      component: Notes,
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/notes.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="v-notes"></main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" src="./notes.scss" scoped />
